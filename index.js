becca4c86ca7d027451562abd50e96d002f24c94
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-25 19:43:17 +0100

    Added timeDiff function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Import external libraries
+ */
+const moment = require('moment');
+
 /** *************************
  * Generial helper functions
  ************************* */
@@ -21,8 +26,45 @@ function getSortOrder(prop) {
   return obj;
 }
 
+function timeDiff(startTime, timeFromNow, addMinutes, displayHrs) {
+  let newStartTime = moment();
+  if (startTime !== null) newStartTime = moment(startTime, 'HH:mm');
+  const newEndTime = moment(timeFromNow, 'HH:mm');
+
+  if (newStartTime.isAfter(newEndTime)) newEndTime.add(1, 'days');
+
+  let addMinutesToTime = 0;
+  if (typeof addMinutes !== 'undefined') addMinutesToTime = addMinutes;
+  newEndTime.add(addMinutesToTime, 'minutes');
+
+  let minutes = newEndTime.diff(newStartTime, 'minutes');
+  if (minutes < 0) minutes = 0;
+  let returnString = `${minutes}`;
+
+  if (displayHrs) {
+    let hours = newStartTime.diff(newEndTime, 'hours');
+    if (hours < 0) hours = 0;
+    if (minutes > 60) {
+      hours = 1;
+      minutes -= 60;
+    }
+    returnString = `${zeroFill(hours, 2)}:${zeroFill(minutes, 2)}`;
+  }
+  return returnString;
+}
+
+function zeroFill(number, width) {
+  const pad = width - number.toString().length;
+  if (pad > 0) {
+    return new Array(pad + (/\./.test(number) ? 2 : 1)).join('0') + number;
+  }
+  return `${number}`; // always return a string
+}
+
 module.exports = {
   // Generial helper functions
   isEmptyObject,
-  GetSortOrder,
+  getSortOrder,
+  timeDiff,
+  zeroFill,
 };
