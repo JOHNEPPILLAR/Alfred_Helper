f6d62e126ac2ffd2d1e1da9f4fa3f59801690330
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-25 19:36:50 +0100

    Adeed getSortOrder function

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ function isEmptyObject(obj) {
   return !Object.keys(obj).length;
 }
 
+function getSortOrder(prop) {
+  const obj = function AB(a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    }
+    if (a[prop] < b[prop]) return -1;
+    return 0;
+  };
+  return obj;
+}
+
 module.exports = {
   // Generial helper functions
   isEmptyObject,
+  GetSortOrder,
 };
