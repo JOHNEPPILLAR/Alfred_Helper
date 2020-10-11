94ad28d0843eb22cd2776beb63b0d9cf8858ab47
JP <johneppillar@gmail.com>
2020-10-11 16:56:46 +0100

    Inprove array check

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ function isEmptyObject(obj) {
   if (obj == null) return true;
   if (obj.length > 0) return false;
   if (obj.length === 0) return true;
-  if (typeof obj !== 'object') return true;
-  return !Object.keys(obj).length;
+  // if (typeof obj !== 'object') return true;
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function getSortOrder(prop) {
