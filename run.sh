1a880e4f08c6014a344c0da9e8d0cfe9067f8943
johneppillar@gmail.com <johneppillar@gmail.com>
2021-04-02 11:06:27 +0100

    Bump libs

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-echo "Installing latest"
-#rm -rf node_modules
-#rm package-lock.json
-ncu -u
-npm install
-npm audit fix
-snyk test
+if [ -z "$1" ] 
+then
+    echo "Skipping re-install"
+else
+    echo "Remove node modules folder and package-lock"
+    rm -rf node_modules
+    rm package-lock.json
+
+    echo "Check for module updates"
+    ncu -u
+
+    echo "Install updates"
+    npm install
+fi
+
+echo "Run snyk"
+npm run test
